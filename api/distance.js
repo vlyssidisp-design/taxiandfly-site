ccd81de1f2ee591dbd283e8398450b26d4b03c0f
vlyssidisp-design <vlyssidisp@gmail.com>
2026-02-15 15:00:33 +0200

    Update distance.js

--- api/distance.js
+++ api/distance.js
@@ -2,21 +2,18 @@ export default async function handler(req, res) {
 
   try {
 
-    const { address } = req.body;
-
     const response = await fetch(
       "https://api.openrouteservice.org/v2/directions/driving-car",
       {
         method: "POST",
         headers: {
-          "Authorization": Authorization: "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjY1NzZjOGUyOTAxYjQxMDNhZmFkZTQ2YTU5ZTM0ZjhiIiwiaCI6Im11cm11cjY0In0="
-,
+          "Authorization": "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjY1NzZjOGUyOTAxYjQxMDNhZmFkZTQ2YTU5ZTM0ZjhiIiwiaCI6Im11cm11cjY0In0=",
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
           coordinates: [
-            [23.9445, 37.9364], // Athens Airport
-            [23.7275, 37.9838]  // Athens center fallback
+            [23.9445, 37.9364],
+            [23.7275, 37.9838]
           ]
         })
       }
